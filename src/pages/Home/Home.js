77fdde660320c9a57ddb132e97402ba08d6c42f5
Salko <64722632+sarajlija@users.noreply.github.com>
2023-05-09 22:05:59 +0200

    up

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -17,6 +17,18 @@ function Home() {
           <img src="/assets/home/desktop/create-and-share.jpg" alt="hero-img" className="img-fluid" />
         </div>
       </section>
+      <section className="section-stories">
+        <div className="image-stories">
+          <img src="/assets/home/desktop/beautiful-stories.jpg" alt="stories-img" className="img-fluid" />
+        </div>
+        <div className="title-stories">
+          <h1>BEAUTIFUL STORIES EVERY TIME</h1>
+          <p>We provide design templates to ensure your stories look terrific. Easily add photos, text, embed maps and media from other networks. Then share your story with everyone.</p>
+          <a href="/" className="get-link">
+            GET AN INVITE
+          </a>
+        </div>
+      </section>
     </main>
   )
 }
