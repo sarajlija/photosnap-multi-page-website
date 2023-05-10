49361e9693c6f44e590b337962814e4177532f4c
Salko <64722632+sarajlija@users.noreply.github.com>
2023-05-10 14:34:33 +0200

    update

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,7 +4,7 @@ import "./Home.css"
 function Home() {
   return (
     <main className="main">
-      <section className="banner-hero">
+      <section className="home-hero">
         <div className="title-hero">
           <h1>Create and share your photo stories. </h1>
           <p>Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others.</p>
@@ -17,7 +17,7 @@ function Home() {
           <img src="/assets/home/desktop/create-and-share.jpg" alt="hero-img" className="img-fluid" />
         </div>
       </section>
-      <section className="section-stories">
+      <section className="home-stories">
         <div className="image-stories">
           <img src="/assets/home/desktop/beautiful-stories.jpg" alt="stories-img" className="img-fluid" />
         </div>
@@ -27,6 +27,73 @@ function Home() {
           <a href="/" className="get-link">
             GET AN INVITE
           </a>
+        </div>
+      </section>
+      <section className="home-designed">
+        <div className="title-designed">
+          <h1>Create and share your photo stories. </h1>
+          <p>Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others.</p>
+          <a href="/" className="get-link">
+            GET AN INVITE
+          </a>
+        </div>
+
+        <div className="image-designed">
+          <img src="/assets/home/desktop/designed-for-everyone.jpg" alt="designed-img" className="img-fluid" />
+        </div>
+      </section>
+      <section className="home-gallery d-flex">
+        <div className="wrapper-image">
+          <img src="/assets/stories/desktop/mountains.jpg" alt="mountains-img" />
+          <div className="card-overlay">
+            <div className="card-title">
+              <h5>The Mountains</h5>
+              <small>by John Appleseed</small>
+            </div>
+            <hr />
+            <div>
+              <a href="/">READ STORE</a>
+            </div>
+          </div>
+        </div>
+        <div className="wrapper-image">
+          <img src="/assets/stories/desktop/cityscapes.jpg" alt="cityscapes-img" />
+          <div className="card-overlay">
+            <div className="card-title">
+              <h5>The Mountains</h5>
+              <small>by John Appleseed</small>
+            </div>
+            <hr />
+            <div>
+              <a href="/">READ STORE</a>
+            </div>
+          </div>
+        </div>
+        <div className="wrapper-image">
+          <img src="/assets/stories/desktop/18-days-voyage.jpg" alt="18-days-voyage-img" />
+          <div className="card-overlay">
+            <div className="card-title">
+              <h5>The Mountains</h5>
+              <small>by John Appleseed</small>
+            </div>
+            <hr />
+            <div>
+              <a href="/">READ STORE</a>
+            </div>
+          </div>
+        </div>
+        <div className="wrapper-image">
+          <img src="/assets/stories/desktop/architecturals.jpg" alt="architecturals-img" />
+          <div className="card-overlay">
+            <div className="card-title">
+              <h5>The Mountains</h5>
+              <small>by John Appleseed</small>
+            </div>
+            <hr />
+            <div>
+              <a href="/">READ STORE</a>
+            </div>
+          </div>
         </div>
       </section>
     </main>
