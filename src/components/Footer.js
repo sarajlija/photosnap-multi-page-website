6ca73b1c5f758d1c5a158482ba3846411c368e1c
Salko <64722632+sarajlija@users.noreply.github.com>
2023-05-12 15:05:40 +0200

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,57 @@
 import React from "react"
-
+import "./Footer.css"
+import ReactFacebook from "../social/facebook.svg"
+import ReactYoutube from "../social/youtube.svg"
+import ReactInstagram from "../social/twitter.svg"
+import ReactPinterest from "../social/instagram.svg"
+import ReactTwitter from "../social/pinterest.svg"
 function Footer() {
-  return <div>Footer</div>
+  return (
+    <footer>
+      <div className="footer-wrapper">
+        <div className="footer-navbar">
+          <div className="navbar-brand">
+            <img className="img-logo" src="/assets/shared/desktop/logo copy.svg" alt="logo" />
+          </div>
+
+          <ul className="footer-nav">
+            <li>home</li>
+            <li>stories</li>
+            <li>features</li>
+            <li>pricing</li>
+          </ul>
+
+          <ul className="social-list">
+            <li>
+              <img src={ReactFacebook} alt="facebook" />
+            </li>
+            <li>
+              <img src={ReactYoutube} alt="youtube" />
+            </li>
+            <li>
+              <img src={ReactTwitter} alt="twitter" />
+            </li>
+            <li>
+              <img src={ReactPinterest} alt="pinterest" />
+            </li>
+            <li>
+              <img src={ReactInstagram} alt="instagram" />
+            </li>
+          </ul>
+        </div>
+        <div className="footer-links">
+          <div>
+            <a href="/" className="get-link">
+              get an invite
+            </a>
+          </div>
+          <div>
+            <p>Copyright 2019. All Rights Reserved</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -96,6 +96,26 @@ function Home() {
           </div>
         </div>
       </section>
+      <section className="home-features d-flex">
+        <div className="features-wrapper d-flex">
+          <div className="features">
+            <img src="/assets/features/desktop/responsive.svg" alt="responsive" />
+            <h5>100% Responsive</h5>
+            <p>No matter which the device you’re on, our site is fully responsive and stories look beautiful on any screen.</p>
+          </div>
+          <div className="features">
+            <img src="/assets/features/desktop/no-limit.svg" alt="no-limit" />
+            <h5>No Photo Upload Limit</h5>
+            <p>Our tool has no limits on uploads or bandwidth. Freely upload in bulk and share all of your stories in one go.</p>
+          </div>
+          <div className="features">
+            <img src="/assets/features/desktop/embed.svg" alt="rembed" />
+            <h5>Available to Embed</h5>
+            <p>Embed Tweets, Facebook posts, Instagram media, Vimeo or YouTube videos, Google Maps, and more. </p>
+          </div>
+        </div>
+        <div></div>
+      </section>
     </main>
   )
 }
